b91ae38e89fbbe39b8e2b73492e2c7667aa0d8ce
Anton Zhilenkov <azhilenkov@tangem.com>
2023-06-26 19:41:43 +0300

    AND-3828 1inch: merged promotion user data into one object. Removed redundant UsedCardsPrefStorage

--- features/learn2earn/impl/src/main/java/com/tangem/feature/learn2earn/data/api/UsedCardsPreferenceStorage.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.feature.learn2earn.data.api
-
-/**
- * @author Anton Zhilenkov on 07.06.2023.
- * An interface that encapsulates access to deprecated PreferencesDataSource.usedCardsPrefStorage.
- */
-interface UsedCardsPreferenceStorage {
-    fun isHadActivatedCards(): Boolean
-}
