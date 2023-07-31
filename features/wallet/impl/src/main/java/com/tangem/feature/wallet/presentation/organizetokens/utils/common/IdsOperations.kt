bc724ccbc517189b6f2e8a77e2f0bf9ed398c43d
iiiburnyiii <iiiburnyiii@gmail.com>
2023-07-31 17:48:12 +0300

    Fixed items IDs for organize tokens items list

--- features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/utils/common/IdsOperations.kt
+++ features/wallet/impl/src/main/java/com/tangem/feature/wallet/presentation/organizetokens/utils/common/IdsOperations.kt
@@ -3,10 +3,14 @@ package com.tangem.feature.wallet.presentation.organizetokens.utils.common
 import com.tangem.domain.tokens.model.Network
 import com.tangem.domain.tokens.model.Token
 
-internal fun getTokenItemId(tokenId: Token.ID): String = tokenId.value
+private const val TOKEN_ITEM_ID_PREFIX = "token_"
+private const val GROUP_HEADER_ID_PREFIX = "group_"
 
-internal fun getGroupHeaderId(networkId: Network.ID): String = networkId.value
+internal fun getTokenItemId(tokenId: Token.ID): String = TOKEN_ITEM_ID_PREFIX + tokenId.value
 
-internal fun getTokenId(tokenItemId: String): Token.ID = Token.ID(tokenItemId)
+internal fun getGroupHeaderId(networkId: Network.ID): String = GROUP_HEADER_ID_PREFIX + networkId.value
 
-internal fun getNetworkId(groupHeaderId: String): Network.ID = Network.ID(groupHeaderId)
+internal fun getTokenId(tokenItemId: String): Token.ID = Token.ID(tokenItemId.substringAfter(TOKEN_ITEM_ID_PREFIX))
+
+internal fun getNetworkId(groupHeaderId: String): Network.ID =
+    Network.ID(groupHeaderId.substringAfter(GROUP_HEADER_ID_PREFIX))
