f4600c8fc30aba30add626922bdf2871900d2e84
Ilya Shulgin <shulgin.ilya.i@gmail.com>
2024-04-15 20:55:38 +0200

    AND-6615 Updated mocks.

--- app/src/main/java/com/tangem/tap/domain/sdk/mocks/wallet/WalletMocks.kt
+++ app/src/main/java/com/tangem/tap/domain/sdk/mocks/wallet/WalletMocks.kt
@@ -4,6 +4,8 @@ import com.tangem.common.card.CardWallet
 import com.tangem.common.card.EllipticCurve
 import com.tangem.common.card.EncryptionMode
 import com.tangem.common.card.FirmwareVersion
+import com.tangem.crypto.hdWallet.DerivationPath
+import com.tangem.crypto.hdWallet.bip32.ExtendedPublicKey
 import com.tangem.domain.models.scan.CardDTO
 import com.tangem.domain.models.scan.ProductType
 import com.tangem.domain.models.scan.ScanResponse
@@ -11,7 +13,7 @@ import com.tangem.operations.attestation.Attestation
 import com.tangem.tap.domain.sdk.mocks.Mocks
 import java.util.Date
 
-object WalletMocks : Mocks() {
+object WalletMocks : Mocks {
 
     private val cardDto = CardDTO(
         cardId = "AC05000000086747",
@@ -85,8 +87,26 @@ object WalletMocks : Mocks() {
                 remainingSignatures = null,
                 index = 0,
                 hasBackup = false,
-                derivedKeys = emptyMap(), // TODO
-                extendedPublicKey = null, // TODO
+                derivedKeys = mapOf(
+                    DerivationPath("m/44'/0'/0'/0/0") to ExtendedPublicKey(
+                        publicKey = byteArrayOf(
+                            3, 45, 58, -110, -52, -51, -83, -4, -45, -118, 119, 37, 123, -17, 66, -83, 61,
+                            -106, 115, 47, 121, 66, 84, -122, -57, -45, 7, -79, 70, -13, 28, -125, -52),
+                        chainCode = byteArrayOf(
+                            93, 51, 52, -66, -39, -38, 34, -84, 50, 1, -127, -20, 80, -20, -30, -72, 2, 1,
+                            -78, -81, -17, 51, -52, -25, 12, 108, 50, 89, -66, 18, 65, 70),
+                    ),
+                    DerivationPath("m/44'/60'/0'/0/0") to  ExtendedPublicKey(
+                        publicKey = byteArrayOf(2, 34, 6, 119, -106, 5, -119, 111, -22, 8, 23, -108, -72, -56, 6, 77, -17, -61, -101, -85, 16, 28, 18, 3, -3, -89, -81, -108, 48, -7, -86, -82, -67),
+                        chainCode = byteArrayOf(
+                            -75, 55, 107, -106, -37, -81, -15, 72, -102, 94, 55, -39, 9, -112, 1, 90, -50, 103, 53, 120, -92, -36, -85, -39, -65, 1, 88, 46, 92, 104, -13, -109),
+                    ),
+                ),
+                extendedPublicKey =ExtendedPublicKey(
+                    publicKey = byteArrayOf(2, -109, 28, -27, -124, -58, -97, -61, 43, -84, 90, -9, -5, 4, 90, 17,
+                        112, -125, -108, 44, 19, -79, -60, -23, 34, -20, -20, 61, 84, 113, 120, -90, -5),
+                    chainCode = byteArrayOf(80, 13, -8, -108, -35, 116, -92, 125, -65, -28, 85, 72, -113, 59, 83, 13, 5, -83, -102, 123, 124, -22, 94, 108, -71, 95, 65, -2, 38, 38, -108, 14),
+                ),
                 isImported = false,
             ),
             CardDTO.Wallet(
@@ -105,7 +125,10 @@ object WalletMocks : Mocks() {
                 index = 1,
                 hasBackup = false,
                 derivedKeys = emptyMap(),
-                extendedPublicKey = null, // TODO
+                extendedPublicKey =ExtendedPublicKey(
+                    publicKey = byteArrayOf(-65, -53, -62, -12, -57, -32, -38, -9, -128, -52, -83, -61, 73, 39, 41, 15, -74, -97, 38, 52, -101, 63, 74, -56, -20, 15, 57, -127, 114, -93, -17, -109),
+                    chainCode = byteArrayOf(-81, 15, 125, 28, -115, -22, 87, 81, 87, -123, -25, -74, 86, 2, 1, 110, -115, 65, -110, 63, -64, 83, -93, -97, -104, 123, 12, -26, 94, 27, 84, -6),
+                ),
                 isImported = false,
             ),
         ),
