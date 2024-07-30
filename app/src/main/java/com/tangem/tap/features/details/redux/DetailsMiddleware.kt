711c3b81ee7d8da4e10796e66ce9cb055a0de151
Vladislav Kozarez <vladks32@gmail.com>
2024-07-30 11:03:53 +0200

    Merge/5.13.0 1 (#3038)

--- app/src/main/java/com/tangem/tap/features/details/redux/DetailsMiddleware.kt
+++ app/src/main/java/com/tangem/tap/features/details/redux/DetailsMiddleware.kt
@@ -154,7 +154,6 @@ class DetailsMiddleware {
             // Nothing to change
             val walletsRepository = store.inject(DaggerGraphState::walletsRepository)
 
-            // TODO: как такое возможно?)
             if (walletsRepository.shouldSaveUserWalletsSync() == enable) {
                 store.dispatchWithMain(DetailsAction.AppSettings.SwitchPrivacySetting.Success)
                 return@launch
@@ -246,7 +245,7 @@ class DetailsMiddleware {
             deleteSavedAccessCodes()
             store.inject(DaggerGraphState::walletsRepository).saveShouldSaveUserWallets(item = false)
 
-            store.dispatchNavigationAction { popTo<AppRoute.Home>() }
+            store.dispatchNavigationAction { replaceAll(AppRoute.Home) }
 
             return CompletionResult.Success(Unit)
         }
